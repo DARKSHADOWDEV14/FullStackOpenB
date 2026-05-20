88d96ce389daf93a6b76f4b86e30bc9f3ea72b3c
Andrew <andresdavid.dev.1405@gmail.com>
2026-05-20 17:32:49 -0500

    empezando nuevo repositorio del curso FullStackOpen

---
+++ part3/index.js
@@ -0,0 +1,60 @@
+import express from 'express'
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+  const quantity = persons.length
+  const date = new Date()
+
+  res.send(`
+    <p>Phonebook has info for ${quantity} people</p>
+    <p>${date}</p>
+  `)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end("no se puede encontrar a la persona")
+  }
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port http://localhost:${PORT}`)
